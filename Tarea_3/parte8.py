4492d59b542301b5df0455546c77cff19bed8167
Benjamín Fernández <bfernandezt@utem.cl>
2025-09-11 21:58:01 -0300

    última parte

---
+++ Tarea_3/parte8.py
@@ -0,0 +1,28 @@
+#Interpreta los resultados de los gráficos y estadísticas: ¿qué conclusiones puedes sacar sobre los Pokémon de la primera generación?
+#Qué tipo de Pokémon sería "más balanceado" según las estadísticas? ¿Y el más especializado?
+
+#1.Histograma del ataque
+#Se observa que la mayoría de los Pokémon tienen ataques en el rango 40 a 90, lo que indica que estos valores son los más comunes.
+#Existen pocos Pokémon con ataques extremadamente bajos (como Magikarp con 10 y Chansey con 5) o muy altos (como Dragonite con 134 y Machamp con 130).
+
+#2. Gráfico de dispersión entre ataque y velocidad
+#No se aprecia una correlación clara: hay Pokémon rápidos con ataques bajos (por ejemplo un punto con velocidad 140 pero ataque 50) y Pokémon lentos con ataque alto (otro punto con ataque 110 pero velocidad 30).
+#La correlación calculada es débil, lo que confirma que tener un ataque alto no implica necesariamente tener velocidad alta.
+
+#3. Distribución de la defensa usando un gráfico de violín
+# La defensa presenta una distribución bimodal, con una gran concentración de valores en torno a 50–80 y un grupo más pequeño de Pokémon con defensas extremadamente altas.
+# La dispersión es bastante amplia, lo que demuestra que la defensa es un atributo donde existen grandes diferencias entre especies.
+
+#4. Gráfico de ataque y defensa de los Pokémon separados por tipo 1
+# Tipos como Lucha y Roca tienden a concentrarse en valores altos de ataque y defensa, mostrando que suelen ser Pokémon más robustos.
+# Tipos como Bicho y Fantasma muestran valores más bajos y dispersos en ambas variables.
+# Psíquico y Eléctrico se ubican en zonas medias, con más equilibrio entre ataque y defensa.
+# Roca/Tierra se destacan en defensa, los Lucha/Fuego en ataque, y los Normal tienen gran variabilidad.
+
+#5. Outliers en ataque y PS por tipo 1 usando boxplot
+# Ataque:
+#Chansey (Normal) con 5 es un outlier muy bajo.
+#Rhydon (Tierra) con 130 es un outlier superior 
+# PS (puntos de salud):
+# Chansey (Normal) con 250 y Snorlax (Normal) con 160 son outliers muy altos, que superan ampliamente al promedio de su tipo.
+
